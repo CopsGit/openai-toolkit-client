04a43cdbdac0dab9c9ef019615674ec1af3bc5c2
copsgit <github@copsking.com>
2022-12-28 19:22:04 -0500

    chatgpt

---
+++ src/components/chatTypes/ChatGpt.jsx
@@ -0,0 +1,141 @@
+import React, {useEffect, useRef, useState} from 'react';
+import sendIcon from '../../assets/send.svg'
+import ChatBlock from "./ChatBlock";
+import '../../pages/chatpage/chatPage.scss'
+import api from "../../utils/axiosSetting";
+// import { ChatGPTAPIBrowser } from 'chatgpt'
+
+const ChatGpt = () => {
+    const [chatData, setChatData] = useState([]);
+    const [inputValue, setInputValue] = useState('');
+    const [uniqueId, setUniqueId] = useState("");
+    const chatRef = useRef(null);
+
+    let loadInterval
+
+    const handleLoader = (element) => {
+        element.textContent = ''
+
+        loadInterval = setInterval(() => {
+            // Update the text content of the loading indicator
+            element.textContent += '.';
+
+            // If the loading indicator has reached three dots, reset it
+            if (element.textContent === '....') {
+                element.textContent = '';
+            }
+        }, 300);
+    }
+
+    const handleTypeResponse = (element, text) => {
+        let index = 0
+
+        let interval = setInterval(() => {
+            if (index < text.length) {
+                element.innerHTML += text.charAt(index)
+                index++
+            } else {
+                clearInterval(interval)
+            }
+        }, 20)
+    }
+
+    const handleUid = () => {
+        const timestamp = Date.now();
+        const randomNumber = Math.random();
+        const hexadecimalString = randomNumber.toString(16);
+
+        return `id-${timestamp}-${hexadecimalString}`;
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const newChatMan =  {
+            isAi: false,
+            value: inputValue,
+            uniqueId: handleUid()
+        }
+
+        // bot's chatstripe
+        const uniqueId = handleUid()
+        setUniqueId(uniqueId)
+
+        const newChatBot = {
+            isAi: true,
+            value: '...',
+            uniqueId
+        }
+
+        setChatData([...chatData, newChatMan, newChatBot])
+
+        // to focus scroll to the bottom
+        chatRef.current.scrollTop = chatRef.current.scrollHeight
+    }
+
+    useEffect(() => {
+        const fetchChat = async () => {
+            // specific message div
+            const messageDiv = document.getElementById(uniqueId)
+
+            console.log(uniqueId,messageDiv)
+
+            handleLoader(messageDiv)
+
+            const response = await api.post('/chat', {
+                prompt: inputValue
+            })
+
+            clearInterval(loadInterval)
+            messageDiv.innerHTML = " "
+
+            // to clear the textarea input
+            setInputValue('')
+
+            console.log(response)
+
+            if (response.status === 200) {
+                const data = await response.data;
+                const parsedData = data?.bot.trim() // trims any trailing spaces/'\n'
+
+                handleTypeResponse(messageDiv, parsedData)
+            } else {
+                const err = await response.data;
+
+                messageDiv.innerHTML = "Something went wrong"
+                alert(err)
+            }
+        }
+
+        if (uniqueId) {
+            fetchChat().then()
+        }
+    }, [chatData]);
+
+
+    return (
+        <>
+            <div id="chat_container" ref={chatRef}>
+                {
+                    chatData?.map((item, index) =>
+                        <ChatBlock key={index} isAi={item.isAi} value={item.value} uniqueId={item.uniqueId}/>
+                    )
+                }
+            </div>
+            <form onSubmit={handleSubmit} onKeyDown={(e) => {if (e.key === 'Enter') {handleSubmit(e).then()}}}>
+                <textarea
+                    name="prompt"
+                    rows="1"
+                    cols="1"
+                    placeholder="Ask davinci..."
+                    required
+                    onChange={(e) => setInputValue(e.target.value)}
+                    value={inputValue}
+                ></textarea>
+                <button type="submit"><img src={sendIcon} alt="send"/></button>
+            </form>
+        </>
+    );
+};
+
+export default ChatGpt;
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Layout from "../../layout/Layout";
 import TextToImg from "../../components/textToImg/TextToImg";
 import ImageVariation from "../../components/imgVariation/ImageVariation";
+import ChatGpt from "../../components/chatTypes/ChatGpt";
 
 const HomePage = () => {
     return (
         <Layout features={["Home"]}>
-            <ImageVariation />
+            <ChatGpt />
         </Layout>
     );
 };
